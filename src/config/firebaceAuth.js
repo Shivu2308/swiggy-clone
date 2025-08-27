b6efa93e826b4cb70a0a031e9b7e6c45879f0e1c
Shivam Singh Rajput <rsraj0981@gmail.com>
2025-08-27 11:28:19 +0530

    first commit

--- src/config/firebaceAuth.js
+++ src/config/firebaceAuth.js
@@ -6,7 +6,14 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = JSON.parse(import.meta.env.VITE_KEY)
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_AUTH-API_KEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+}
 
 
 // Initialize Firebase
